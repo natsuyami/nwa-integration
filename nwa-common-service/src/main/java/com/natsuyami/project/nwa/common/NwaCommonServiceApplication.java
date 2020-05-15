807a7c2d082f444876bd80cee24a9bd38afacd0e
Ryan Gabriel Bunquin <bunquinryan@gmail.com>
2020-05-15 21:13:19 +0800

    Intial Commit

---
+++ nwa-common-service/src/main/java/com/natsuyami/project/nwa/common/NwaCommonServiceApplication.java
@@ -0,0 +1,13 @@
+package com.natsuyami.project.nwa.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NwaCommonServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NwaCommonServiceApplication.class, args);
+	}
+
+}
---
+++ nwa-common-service/src/test/java/com/natsuyami/project/nwa/common/NwaCommonServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.natsuyami.project.nwa.common;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NwaCommonServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
